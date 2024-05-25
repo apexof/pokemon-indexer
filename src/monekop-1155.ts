0c247a63fb3e0e170386108f9c8d880298895834
AlexOrekhov <apexof@gmail.com>
2024-05-25 17:00:52 +0400

    add counter

--- src/monekop-1155.ts
+++ src/monekop-1155.ts
@@ -1,21 +1,16 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import { TransferSingle as TransferSingleEvent } from "../generated/Monekop1155/Monekop1155"
-import { TransferSingle } from "../generated/schema"
+import { User } from "../generated/schema"
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.Monekop1155_id = event.params.id
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  const userAddress = event.params.to.toHex()
+  let user = User.load(userAddress)
+  if (!user){
+    user = new User(userAddress)
+    user.pokemonsCount = BigInt.fromI32(0);
+  }
+  user.pokemonsCount = user.pokemonsCount.plus(BigInt.fromI32(1));
+  user.save()
 }
 
 
