47c57b3fd29020b9dfbf18929c9ee210834c222d
AlexOrekhov <apexof@gmail.com>
2024-05-25 17:35:22 +0400

    add pokemons arrat

--- src/monekop-1155.ts
+++ src/monekop-1155.ts
@@ -1,17 +1,25 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { TransferSingle as TransferSingleEvent } from "../generated/Monekop1155/Monekop1155"
-import { User } from "../generated/schema"
+import { User, Pokemon } from "../generated/schema"
 
 export function handleTransferSingle(event: TransferSingleEvent): void {
   const userAddress = event.params.to.toHex()
   let user = User.load(userAddress)
-  if (!user){
+
+  if (!user) {
     user = new User(userAddress)
-    user.pokemonsCount = BigInt.fromI32(0);
+    user.pokemonsCount = BigInt.fromI32(0)
   }
-  const pokemonId = event.params.id
-  user.pokemonsCount = user.pokemonsCount.plus(BigInt.fromI32(1));
+
+  const pokemonId = event.params.id.toString()
+  let pokemon = Pokemon.load(pokemonId)
+
+  if (!pokemon) {
+    pokemon = new Pokemon(pokemonId)
+    pokemon.name = "DefaultName" 
+    pokemon.owner = user.id
+    pokemon.save()
+  }
+  user.pokemonsCount = user.pokemonsCount.plus(BigInt.fromI32(1))
   user.save()
 }
-
-
